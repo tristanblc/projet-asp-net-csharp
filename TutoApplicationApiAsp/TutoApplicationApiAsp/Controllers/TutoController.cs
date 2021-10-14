99546c103b0be4a80bcfb503e0e4d7f2b87ae42b
Tristan Blanc <55233213+tristanblc@users.noreply.github.com>
2021-10-14 17:11:05 +0200

    fin du projet
    fin du projet

---
+++ TutoApplicationApiAsp/TutoApplicationApiAsp/Controllers/TutoController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+namespace TutoApplicationApiAsp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TutoController : Controller
+    {
+
+        private readonly ILogger<TutoController> _logger;
+
+        public TutoController(ILogger<TutoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Tuto> Get()
+        {
+            string cs = @"server=localhost;userid=root;password=root;database=bdformation;SSL Mode=None";
+            MySqlConnection con = new MySqlConnection(cs);
+            con.Open();
+
+            string sql = "SELECT title,price,img,description from tutos";
+            using var cmd = new MySqlCommand(sql, con);
+            using MySqlDataReader rcr = cmd.ExecuteReader();
+
+            int nbTuto = 6;
+            Tuto[] formations = new Tuto[nbTuto];
+
+            int cpt = 0;
+
+            while (rcr.Read() && cpt < nbTuto)
+            {
+                Tuto t = new Tuto { titre = rcr[0].ToString(), price = (int) rcr[1], img = rcr[0].ToString(), desc = rcr[0].ToString() };
+                
+                formations[cpt] = t;
+                cpt++;
+            }
+
+            return formations;
+        }
+        [HttpGet]
+        [Route("index")]
+        public Tuto GetByIndex(int i)
+        {
+            string cs = @"server=localhost;userid=root;password=root;database=bdformation";
+            MySqlConnection con = new MySqlConnection(cs);
+            con.Open();
+
+            string sql = "SELECT title,price,img,description from tutos";
+            using var cmd = new MySqlCommand(sql, con);
+            using MySqlDataReader rcr = cmd.ExecuteReader();
+
+            int nbTuto = 6;
+            Tuto[] formations = new Tuto[nbTuto];
+
+            int cpt = 0;
+
+            while (rcr.Read() && cpt < nbTuto)
+            {
+                Tuto t = new Tuto { titre = rcr[0].ToString(), price = (int)rcr[1], img = rcr[0].ToString(), desc = rcr[0].ToString() };
+
+                formations[cpt] = t;
+                cpt++;
+            }
+
+            return formations[i];
+  
+        }
+
+
+
+
+
+
+    }
+}
---
+++ TutoApplicationApiAsp/TutoApplicationApiAsp/Tuto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TutoApplicationApiAsp
+{
+    public class Tuto
+    {
+
+        public string titre { get; set; }
+        public int price { get; set; }
+        public string img { get; set; }
+        public string desc { get; set; }
+
+
+        public override string ToString()
+        {
+            return  titre +" | "+ price + " | " + img + " | " + desc ;
+        }
+
+
+    }
+}
